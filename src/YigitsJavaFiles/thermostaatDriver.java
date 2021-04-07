049cf96b5472c3f763e6e4b9246e15ac1232c9e7
Yigit <yigit8@live.nl>
2021-04-07 14:13:23 +0200

    array thermostat loop

--- src/YigitsJavaFiles/thermostaatDriver.java
+++ src/YigitsJavaFiles/thermostaatDriver.java
@@ -12,36 +12,64 @@ public class thermostaatDriver {
         float tempStepSize;
         float tempNightTemp;
         float tempDayTemp;
-
+        int maxSize;
         Scanner in = new Scanner(System.in);
 
-        in.nextLine();
-        System.out.print("bool: ");
-        tempOperational = in.nextBoolean();
+        maxSize = 0;
+        do {
+            try {
+                System.out.println("Enter the number of thermostats you would like to add Enter 0 (zero) if you do not wish to add products: ");
+                maxSize = in.nextInt();
 
-        System.out.print("bool: ");
-        tempProgActive = in.nextBoolean();
+                if (maxSize == 0) {
+                    System.out.println("No thermostat(s) required!");
+                     System.exit(0);
+                }
+                else {
+                    thermostaat[] arrayThermostat = new thermostaat[maxSize];
+                    for (int a = 0; a < arrayThermostat.length; a++){
 
-        System.out.print("float: ");
-        tempMinTemp = in.nextFloat();
+                        in.nextLine();
+                        System.out.print("bool: ");
+                        tempOperational = in.nextBoolean();
 
-        System.out.print("float: ");
-        tempMaxTemp = in.nextFloat();
+                        System.out.print("bool: ");
+                        tempProgActive = in.nextBoolean();
 
-        System.out.print("float: ");
-        tempCurTemp = in.nextFloat();
+                        System.out.print("float: ");
+                        tempMinTemp = in.nextFloat();
 
-        System.out.print("float: ");
-        tempStepSize = in.nextFloat();
+                        System.out.print("float: ");
+                        tempMaxTemp = in.nextFloat();
 
-        System.out.print("float: ");
-        tempNightTemp = in.nextFloat();
+                        System.out.print("float: ");
+                        tempCurTemp = in.nextFloat();
 
-        System.out.print("float: ");
-        tempDayTemp = in.nextFloat();
+                        System.out.print("float: ");
+                        tempStepSize = in.nextFloat();
 
-        thermostaat GOKU = new thermostaat(tempOperational, tempProgActive, tempMinTemp, tempMaxTemp, tempCurTemp, tempStepSize, tempNightTemp, tempDayTemp);
-        System.out.println(GOKU.toString());
+                        System.out.print("float: ");
+                        tempNightTemp = in.nextFloat();
+
+                        System.out.print("float: ");
+                        tempDayTemp = in.nextFloat();
+
+                        arrayThermostat[a] = new thermostaat(tempOperational, tempProgActive, tempMinTemp, tempMaxTemp, tempCurTemp, tempStepSize, tempNightTemp, tempDayTemp);
+                    }
+
+                    System.out.println("Here are your products: \r\n");
+
+                    for (int i = 0; i < arrayThermostat.length; i++) {
+                        System.out.println(arrayThermostat[i].toString());
+                    }
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Incorrect, you didnt give an int, the original error: " + e);
+                in.nextLine();
+            }
+        }
+        while (maxSize <= 0);
     }
 
 }
